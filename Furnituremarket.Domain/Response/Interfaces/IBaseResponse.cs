bf82e9e44540efa5a7c4a64de52024b9a9fc31e7
STGorbunovDA <bous07@mail.ru>
2023-07-10 20:00:58 +0800

    Создать IBaseResponse

---
+++ Furnituremarket.Domain/Response/Interfaces/IBaseResponse.cs
@@ -0,0 +1,11 @@
+﻿using Furnituremarket.Domain.Enum;
+
+namespace Furnituremarket.Domain.Response.Interfaces
+{
+    public interface IBaseResponse<T>
+    {
+        public string Description { get; }
+        public StatusCode CodeStatus { get; }
+        T Data { get; }
+    }
+}
