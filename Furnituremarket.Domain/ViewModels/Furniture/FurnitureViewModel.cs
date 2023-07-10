e347f8131d241999561072ae6b68d81448ac2d9b
STGorbunovDA <bous07@mail.ru>
2023-07-10 20:07:08 +0800

    Создать FurnitureViewModel

---
+++ Furnituremarket.Domain/ViewModels/Furniture/FurnitureViewModel.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+
+namespace Furnituremarket.Domain.ViewModels.Furniture
+{
+    public class FurnitureViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Color { get; set; }
+        public string Material { get; set; }
+        public decimal Price { get; set; }
+        public DateTime DateCreate { get; set; }
+        public IFormFile Avatar { get; set; }
+    }
+}
