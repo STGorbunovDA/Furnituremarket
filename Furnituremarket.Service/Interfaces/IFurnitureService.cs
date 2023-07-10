8d1e44511f6a4dc5dccfbaf1eebb7d574052286c
STGorbunovDA <bous07@mail.ru>
2023-07-10 20:07:38 +0800

    Создать IFurnitureService

---
+++ Furnituremarket.Service/Interfaces/IFurnitureService.cs
@@ -0,0 +1,26 @@
+﻿using Furnituremarket.Domain.Model;
+using Furnituremarket.Domain.Response.Interfaces;
+using Furnituremarket.Domain.ViewModels.Furniture;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Furnituremarket.Service.Interfaces
+{
+    public interface IFurnitureService
+    {
+        Task<IBaseResponse<bool>> CreateFurniture(
+            FurnitureViewModel furnitureViewModel);
+
+        Task<IBaseResponse<IEnumerable<Furniture>>> GetAllFurniture();
+
+        Task<IBaseResponse<bool>> UpdateFurniture(int id,
+            FurnitureViewModel furnitureViewModel);
+
+        Task<IBaseResponse<bool>> DeleteFurniture(int id);
+
+        Task<IBaseResponse<Furniture>> GetFurnitureById(int id);       
+
+        Task<IBaseResponse<IEnumerable<Furniture>>> GetFurnitureByName(string name);
+        
+    }
+}
