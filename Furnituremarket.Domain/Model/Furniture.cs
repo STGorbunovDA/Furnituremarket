a80ab992ce37155569fa9a1e0b0d36edd2587147
STGorbunovDA <bous07@mail.ru>
2023-07-10 19:14:50 +0800

    Добавить дату создания мебели

--- Furnituremarket.Domain/Model/Furniture.cs
+++ Furnituremarket.Domain/Model/Furniture.cs
@@ -1,23 +1,28 @@
-﻿namespace Furnituremarket.Domain.Model
+﻿using System;
+
+namespace Furnituremarket.Domain.Model
 {
     public class Furniture
     {
         public int Id { get; set; }
-        public string NameTitle { get; set; }
+        public string Name { get; set; }
         public string Description { get; set; }
         public string Color { get; set; }
         public string Material { get; set; }
         public decimal Price { get; set; }
+        public DateTime DateCreate { get; set; }
 
-        public Furniture(int id, string nameTitle, string description,
-            string color, string material, decimal price)
+        public Furniture(int id, string name, string description,
+            string color, string material, decimal price, DateTime dateCreate)
         {
             Id = id;
-            NameTitle = nameTitle;
+            Name = name;
             Description = description;
             Color = color;
             Material = material;
             Price = price;
+            DateCreate = dateCreate;
         }
+        public Furniture() { }
     }
 }
