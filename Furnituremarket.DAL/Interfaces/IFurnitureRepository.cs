7d85dae714018653e8662cdebaacb47faf6e4f00
STGorbunovDA <bous07@mail.ru>
2023-07-10 19:16:35 +0800

    Добавить интерфейс поиск по ID и name (IFurnitureRepository)

---
+++ Furnituremarket.DAL/Interfaces/IFurnitureRepository.cs
@@ -0,0 +1,12 @@
+﻿using Furnituremarket.Domain.Model;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Furnituremarket.DAL.Interfaces
+{
+    public interface IFurnitureRepository : IBaseRepository<Furniture>
+    {
+        Task<List<Furniture>> GetByName(string name);
+        Task<Furniture> GetById(int id);
+    }
+}
