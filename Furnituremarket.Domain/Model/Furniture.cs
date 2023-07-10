a8a210b08f844a6d03aa19d26615a7c7a1702c2c
STGorbunovDA <bous07@mail.ru>
2023-07-10 18:52:51 +0800

    Описать сущность Furniture

---
+++ Furnituremarket.Domain/Model/Furniture.cs
@@ -0,0 +1,23 @@
+﻿namespace Furnituremarket.Domain.Model
+{
+    public class Furniture
+    {
+        public int Id { get; set; }
+        public string NameTitle { get; set; }
+        public string Description { get; set; }
+        public string Color { get; set; }
+        public string Material { get; set; }
+        public decimal Price { get; set; }
+
+        public Furniture(int id, string nameTitle, string description,
+            string color, string material, decimal price)
+        {
+            Id = id;
+            NameTitle = nameTitle;
+            Description = description;
+            Color = color;
+            Material = material;
+            Price = price;
+        }
+    }
+}
