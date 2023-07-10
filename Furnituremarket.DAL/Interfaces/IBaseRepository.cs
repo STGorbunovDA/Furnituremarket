c82692141c9e472306ea8c284115b53e23c393f1
STGorbunovDA <bous07@mail.ru>
2023-07-10 19:15:56 +0800

    Добавить интерфейс CRUD (IBaseRepository)

---
+++ Furnituremarket.DAL/Interfaces/IBaseRepository.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Furnituremarket.Domain.Model;
+
+namespace Furnituremarket.DAL.Interfaces
+{
+    public interface IBaseRepository<T>
+    {
+        Task<bool> Create(T entity);
+        Task<List<Furniture>> Read();
+        Task<bool> Update(T entity);
+        Task<bool> Delete(T entity);
+
+    }
+}
