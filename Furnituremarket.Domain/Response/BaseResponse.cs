3f1f6e4489147d159f564283fb3b08b8b2fae515
STGorbunovDA <bous07@mail.ru>
2023-07-10 20:01:08 +0800

    Реализовать BaseResponse

---
+++ Furnituremarket.Domain/Response/BaseResponse.cs
@@ -0,0 +1,12 @@
+﻿using Furnituremarket.Domain.Enum;
+using Furnituremarket.Domain.Response.Interfaces;
+
+namespace Furnituremarket.Domain.Response
+{
+    public class BaseResponse<T> : IBaseResponse<T>
+    {
+        public string Description { get; set; }
+        public StatusCode CodeStatus { get; set; }
+        public T Data { get; set; }
+    }
+}
