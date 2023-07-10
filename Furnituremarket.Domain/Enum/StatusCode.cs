d5c061924ff2a417f4ebbe3df40acf4f9f48c565
STGorbunovDA <bous07@mail.ru>
2023-07-10 20:00:35 +0800

    Создать StatusCode

---
+++ Furnituremarket.Domain/Enum/StatusCode.cs
@@ -0,0 +1,12 @@
+﻿namespace Furnituremarket.Domain.Enum
+{
+    public enum StatusCode
+    {
+        FurnitureNotFound = 0,
+        CreateNotFound = 1,
+        DeleteNotFound = 2,
+        UpdateNotFound = 3,
+        OK = 200,
+        InternalServerError = 500,
+    }
+}
