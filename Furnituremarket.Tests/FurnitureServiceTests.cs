ebd4b289654991ffc175711a376004ea481513cf
STGorbunovDA <bous07@mail.ru>
2023-07-12 00:47:14 +0800

    #3

--- Furnituremarket.Tests/FurnitureServiceTests.cs
+++ Furnituremarket.Tests/FurnitureServiceTests.cs
@@ -1,6 +1,8 @@
 ﻿using Furnituremarket.DAL.Interfaces;
 using Furnituremarket.Domain.Model;
+using Furnituremarket.Service.Implementations;
 using Moq;
+using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using Xunit;
@@ -9,15 +11,22 @@ namespace Furnituremarket.Tests
 {
     public class FurnitureServiceTests
     {
-        [Fact]
-        public void GetFurniture_WithName_CallsGetByQuery()
-        {
-            //List<Furniture> listFurniture = new List<Furniture>();
-            //var furnitureRepositoryStub = new Mock<IFurnitureRepository>();
-            //furnitureRepositoryStub.Setup(x => x.GetByQuery(It.IsAny<string>()))
-            //    .Returns(
-            //         listFurniture.Add(new Furniture())
-            //    );
-        }
+    //    [Fact]
+    //    public void GetFurniture_WithName_CallsGetByQuery()
+    //    {
+    //        var furnitureRepositoryStub = new Mock<IFurnitureRepository>();
+
+    //        furnitureRepositoryStub.Setup(x => x.GetByQuery(It.IsAny<string>()))
+    //            .Returns(() => new Task<List<Furniture>>(() =>
+    //                    new List<Furniture>() { new Furniture(1, "Стол", "", "", "", 0.00m, DateTime.Now) }));
+           
+    //        var furnitureService = new FurnitureService(furnitureRepositoryStub.Object);
+
+    //        var validName = "Стол";
+
+    //        var actual = furnitureService.GetFurniture(validName);
+
+    //        Assert.Collection ((IEnumerable<Furniture>) actual, furniture => Assert.Equal("Стол", furniture.Name));
+    //    }
     }
 }
