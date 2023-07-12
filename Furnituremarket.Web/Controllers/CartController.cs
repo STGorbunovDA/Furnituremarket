96fc9d40af57e3ea1591d9d7c220c1373cb20e56
STGorbunovDA <bous07@mail.ru>
2023-07-13 02:35:44 +0800

    Перенести cart в слой domain, перенести метод AddInCartFurniture в отдельный CartContoller

---
+++ Furnituremarket.Web/Controllers/CartController.cs
@@ -0,0 +1,26 @@
+﻿using Furnituremarket.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Furnituremarket.Web.Controllers
+{
+    public class CartController : Controller
+    {
+        [HttpPost]
+        public IActionResult AddInCartFurniture(int id, decimal price)
+        {
+            Cart cart;
+
+            if (!HttpContext.Session.TryGetCart(out cart))
+                cart = new Cart();
+
+            if (cart.Items.ContainsKey(id)) cart.Items[id]++;
+            else cart.Items[id] = 1;
+
+            cart.Amount += price;
+
+            HttpContext.Session.Set(cart);
+
+            return RedirectToAction("GetAllFurniture", "Furniture", new { id });
+        }
+    }
+}
