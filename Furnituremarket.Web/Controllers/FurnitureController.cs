1e4437dd314027e474ba3592dce9225c610519ef
STGorbunovDA <bous07@mail.ru>
2023-07-16 07:50:06 +0800

    #19 Реализовать загрузку картинки карточки мебели

--- Furnituremarket.Web/Controllers/FurnitureController.cs
+++ Furnituremarket.Web/Controllers/FurnitureController.cs
@@ -1,11 +1,6 @@
 ﻿using Furnituremarket.Domain.Model;
-using Furnituremarket.Domain.Response.Interfaces;
 using Furnituremarket.Service.Interfaces;
-using Furnituremarket.Web.Models;
-using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using System;
-using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -57,28 +52,14 @@ namespace Furnituremarket.Web.Controllers
             return RedirectToAction("Error");
         }
 
-
-
         //[Authorize(Roles = "Admin")]
         [HttpPost]
         public async Task<IActionResult> Save(Furniture model)
         {
             if (ModelState.IsValid)
             {
-                if (model.Id == 0)
-                {
-                    //byte[] imageData;
-                    //using (var binaryReader = new BinaryReader(model.Avatar.OpenReadStream()))
-                    //{
-                    //    imageData = binaryReader.ReadBytes((int)model.Avatar.Length);
-                    //}
-                    //await _carService.Create(model, imageData);
-                    await _furnitureService.CreateFurniture(model);
-                }
-                else
-                {
-                    await _furnitureService.UpdateFurniture(model.Id, model);
-                }
+                if (model.Id == 0) await _furnitureService.CreateFurniture(model);
+                else await _furnitureService.UpdateFurniture(model.Id, model);
                 return RedirectToAction("GetAllFurniture");
             }
             return View();
@@ -93,17 +74,13 @@ namespace Furnituremarket.Web.Controllers
             if (id == 0)
                 return View(new Furniture());
 
-
             var response = await _furnitureService.GetFurnitureById(id);
 
             if (response.CodeStatus == Domain.Enum.StatusCode.OK)
                 return View(response.Data);
+
             return RedirectToAction("Error");
         }
-
-
-
-
 
     }
 }
